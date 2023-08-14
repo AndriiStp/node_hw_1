73c3c3c302d87274d4b11fda572478c177f19e4d
Andrii Stepanenko <andrii.stp@gmail.com>
2023-08-14 14:31:30 +0300

    final

--- contacts.js
+++ contacts.js
@@ -1,6 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
-const contactsPath = path.join(__dirname, "/db/contacts.json ");
+const contactsPath = path.join(__dirname, "db/contacts.json");
 const { nanoid } = require("nanoid");
 
 async function listContacts() {
@@ -9,17 +9,20 @@ async function listContacts() {
 }
 
 async function getContactById(contactId) {
-  const contacts = listContacts();
+  const contacts = await listContacts();
   const result = contacts.find((contact) => contact.id === contactId);
   return result || null;
 }
 
 async function removeContact(contactId) {
-  const contacts = listContacts();
+  const contacts = await listContacts();
   const index = contacts.findIndex((contact) => contact.id === contactId);
   if (index === -1) {
     return null;
   }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 3));
+  return result;
 }
 
 async function addContact(name, email, phone) {
