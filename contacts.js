a9341abb9bfd97e33a0a9c6b1420308506f0181e
Andrii Stepanenko <andrii.stp@gmail.com>
2023-08-14 14:17:58 +0300

    12

--- contacts.js
+++ contacts.js
@@ -1,1 +1,43 @@
-const fs = require("fs").promises;
+const fs = require("fs/promises");
+const path = require("path");
+const contactsPath = path.join(__dirname, "/db/contacts.json ");
+const { nanoid } = require("nanoid");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 3));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log('Hello');
+const contacts = require("./contacts.js");
+const { Command } = require("commander");
+
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id, <type>", "user id")
+  .option("-n --name, <type>", "user name")
+  .option("-e, --email, <type>", "user email")
+  .option("-p, --phone, <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const listContacts = await contacts.listContacts();
+      return console.table(listContacts);
+
+    case "get":
+      const getContact = await contacts.getContactById(id);
+      return console.table(getContact);
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      return console.table(newContact);
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      return console.table(removeContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
